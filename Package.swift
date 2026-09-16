4f89b559916fe7fbdc09e3c4db8a89a2f63feb42
jmliu6 <jmliu6@iflytek.com>
2026-09-16 10:29:40 +0800

    保存历史版本准备改动（已由正式6.3.5配置取代）

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YTIFLYADLib",
-            url: "https://github.com/LJMcarryu/YTIFLYADLib_iOS/releases/download/6.3.1/YTIFLYADLib.xcframework.zip",
+            url: "https://github.com/LJMcarryu/YTIFLYADLib_iOS/releases/download/6.3.5/YTIFLYADLib.xcframework.zip",
             checksum: "7adf06f9c3f1d6fe915679322ccb941ba9122edf496c9815db12db3f4e459855"
         ),
         .target(
